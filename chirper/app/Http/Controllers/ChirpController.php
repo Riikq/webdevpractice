975283d7f67d8a17fc8617d9280d707416db0b8a
Riikq <tariqahmad0510@gmail.com>
2026-05-20 11:07:09 +0700

    lets do it

--- chirper/app/Http/Controllers/ChirpController.php
+++ chirper/app/Http/Controllers/ChirpController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Chirp;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\Request;
 
 class ChirpController extends Controller
 {
+    use AuthorizesRequests;
     /**
      * Display a listing of the resource.
      */
@@ -44,9 +46,7 @@ class ChirpController extends Controller
 
         ]);
 
-        Chirp::create([
-            'message' => $validated['message'],
-        ]);
+        auth()->user()->chirps()->create($validated);
 
         return redirect('/')->with('success', 'Your chirp has been posted!');
     }
@@ -64,6 +64,7 @@ class ChirpController extends Controller
      */
     public function edit(Chirp $chirp)
     {
+        $this->authorize('update', $chirp);
         //
         return view('components.chirps.edit', compact('chirp'));
     }
@@ -73,6 +74,7 @@ class ChirpController extends Controller
      */
     public function update(Request $request, Chirp $chirp)
     {
+        $this->authorize('update', $chirp);
         //
         $validated = $request->validate([
             'message' => 'required|string|max:255',
@@ -92,6 +94,7 @@ class ChirpController extends Controller
      */
     public function destroy(Chirp $chirp)
     {
+        $this->authorize('update', $chirp);
         //
         $chirp->delete();
 
